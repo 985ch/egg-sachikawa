b67ca61b8affebf3f615c3759f60892aa00438ef
陈昊 <2972785641@qq.com>
2019-12-17 11:49:13 +0800

    优化返回处理

--- app/extend/context.js
+++ app/extend/context.js
@@ -3,30 +3,29 @@
 const _ = require('lodash');
 const { FastFailError } = require('../../lib/errors');
 
-function setBody(code, msg, data) {
-  return { code, msg, data };
-}
-
 module.exports = {
+  setBody(code, msg, data) {
+    return { code, msg, data };
+  },
   success(data, msg) {
     if (!_.isUndefined(this.body)) {
       return this.logger.error('ctx.body不可重复设置');
     }
-    this.body = setBody(this.app.errCode.SUCCESS, msg, data);
+    this.body = this.setBody(this.app.errCode.SUCCESS, msg, data);
   },
   fail(msg, code, data) {
     if (_.isUndefined(code)) {
       if (_.isNumber(msg)) {
-        this.body = setBody(msg, '服务器错误', code);
+        this.body = this.setBody(msg, '服务器错误', code);
       } else if (_.isUndefined(msg)) {
-        this.body = setBody(-1, '服务器错误');
+        this.body = this.setBody(this.app.errCode.UNKNOWN, '服务器错误');
       } else {
-        this.body = setBody(-1, msg);
+        this.body = this.setBody(this.app.errCode.UNKNOWN, msg);
       }
     } else if (_.isNumber(code)) {
-      this.body = { code, msg, data };
+      this.body = this.setBody(code, msg, data);
     } else {
-      this.body = setBody(-1, msg, code);
+      this.body = this.setBody(this.app.errCode.UNKNOWN, msg, code);
     }
   },
   throwFail(msg, code, data) {
